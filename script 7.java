16557f40093584ba128c3e25fba8ec1495aac872
Basti <basti.babaeva@yandex.ru>
2024-10-24 23:18:52 +0700

    создание методов, добавляющие треугольники и круги

--- script 7.java
+++ script 7.java
@@ -30,19 +30,59 @@ function draw_square(number) {
         square.style.top = y + 'px';
         square.style.width = size + 'px';
         square.style.height = size + 'px';
-        square.style.backgroundColor = '#FF1A1A';
+        square.style.backgroundColor = 'red';
         let isFirstClick = true; // Флаг для отслеживания первого клика
 
-        square.addEventListener('click', function () {
-            if (isFirstClick) {
-                square.style.backgroundColor = '#FFFF1A';
-                isFirstClick = false;
-            } else {
-                square.remove();
-            }
-        });
-
-        
+        drawField.appendChild(square);
     }
 }
+
+function draw_circle(number) {
+    const drawField = document.querySelector('.draw-field');
+
+    for (let i = 0; i < number; i++) {
+        const circle = document.createElement('div');
+        circle.classList.add('circle');
+
+        const size = Math.random() * (300 - 50) ; // Размер от 50 до 300
+        const x = Math.random() * (1800 - size);
+        const y = (Math.random() * (800 - size));
+
+        circle.style.position = 'absolute';
+        circle.style.left = x + 'px';
+        circle.style.top = y + 'px';
+        circle.style.width = size + 'px';
+        circle.style.height = size + 'px';
+        circle.style.backgroundColor = 'green';
+        circle.style.borderRadius='50%';
+        let isFirstClick = true; // Флаг для отслеживания первого клика
+
+        drawField.appendChild(circle);
+    }
+}
+
+function draw_triangle(number) {
+    const drawField = document.querySelector('.draw-field');
+
+    for (let i = 0; i < number; i++) {
+        const triangle = document.createElement('div');
+        triangle.classList.add('triangle');
+
+        const size = Math.random() * (300 - 50) ; // Размер от 50 до 300
+        const x = Math.random() * (1800 - size);
+        const y = (Math.random() * (800 - size));
+
+        triangle.style.position = 'absolute';
+        triangle.style.left = x + 'px';
+        triangle.style.top = y + 'px';
+        triangle.style.borderLeft = size/2 + 'px solid transparent';
+        triangle.style.borderRight = size/2 + 'px solid transparent';
+        triangle.style.borderBottom= size + 'px solid #1A1AFF';
+        let isFirstClick = true; // Флаг для отслеживания первого клика
+
+        drawField.appendChild(triangle);
+    }
+}
+
+
 
