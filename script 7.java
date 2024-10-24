2c16810bbd9cec630b928199347eb10bffd3a2fc
Basti <basti.babaeva@yandex.ru>
2024-10-24 20:33:39 +0700

    создания функций с обработчиком событий click и взятием числа при вводе

--- script 7.java
+++ script 7.java
@@ -1,1 +1,15 @@
+document.addEdventListener('DOMContentLoaded', function(){
+    document.querySelector(".squery").addEdventListener("click", function(){
+        draw_squery(get_number());
+    });
+    document.querySelector(".circle").addEdventListener("click", function(){
+        draw_squery(get_number());
+    });
+    document.querySelector("triangle").addEdventListener("click", function(){
+        draw_squery(get_number());
+    });
+});
 
+function get_number(){
+    return Number(document.querySelector('input').value)||0;
+}
